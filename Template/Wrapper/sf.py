4b1bf497f0c03dec17c0c9a19507d5cc2c441e31
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-01-11 22:27:38 +0700

    make a shorter function by wraping the pygame function

---
+++ Template/Wrapper/sf.py
@@ -0,0 +1,8 @@
+from Kernel import kernal_Init
+kernal_Init.init()
+
+screen = pygame.display.set_mode
+get_events = pygame.event.get
+draw_rect = pygame.draw.rect
+draw_circle = pygame.draw.circle
+setscreen_caption = pygame.display.set_caption
