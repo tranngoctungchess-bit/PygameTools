0a25b29e5b404575d3d6025f47d73b8a050f541d
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-01-25 21:33:24 +0700

    add some tools

--- Kernel/ObjType.py
+++ Kernel/ObjType.py
@@ -1,3 +1,7 @@
 from typing import Tuple, Union
+from collections import namedtuple
+def createpairpack(Objname,valname1, valname2):
+    return namedtuple(Objname, [valname1, valname2])
 MathVal1 = Tuple[Union[int, float],Union[int, float],Union[int, float],Union[int, float]]
 MathVal2 = Tuple[Union[int, float],Union[int, float]]
+Border = createpairpack('Border', 'border_width', 'border_col')
