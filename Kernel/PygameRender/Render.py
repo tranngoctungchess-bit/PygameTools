f5ef49313b2fd88f84cb213509e6aa73ea6caa24
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-01-28 20:25:52 +0700

    add corner radius to border

--- Kernel/PygameRender/Render.py
+++ Kernel/PygameRender/Render.py
@@ -1,5 +1,8 @@
 import pygame
+
+from Kernel import text_Is_Bold, text_Is_Italic, text_is_underline, text_Is_strikethrough, text_Is_Antialias
 from Kernel.VFlags import *
+from Kernel.UFlags import *
 from Kernel.KernelWidget import Widget
 from Kernel.ObjType import *
 def fill_bg(widget: Widget, screen):
@@ -8,9 +11,25 @@ def fill_bg(widget: Widget, screen):
     widget.dirty_vflags.remove(bg_color)
 def set_border(widget: Widget, screen):
     valpack = widget.vflags[border]
-    print(valpack)
-    bg_col = valpack.border_col
-    bg_width = valpack.border_width
-    print(bg_col, bg_width)
-    pygame.draw.rect(screen, bg_col, widget.get_rect(), bg_width)
+    if corner_radius in widget.vflags:
+        pygame.draw.rect(screen, valpack.border_col, widget.get_rect(), valpack.border_width, widget.vflags[corner_radius])
+    else:
+        pygame.draw.rect(screen, valpack.border_col, widget.get_rect(), valpack.border_width)
     widget.dirty_vflags.remove(border)
+    widget.dirty_vflags.remove(corner_radius)
+def draw_text(widget: Widget, screen):
+    valpack: TextPack = widget.vflags[textpack]
+    font = pygame.font.SysFont(valpack.Font, valpack.Size, text_Is_Bold in widget.uflags, text_Is_Italic in widget.uflags)
+    if text_Is_Underline in widget.uflags:
+        font.set_underline(True)
+    if text_Is_Strikethrough in widget.uflags:
+        font.set_strikethrough(True)
+    if text_have_background in widget.uflags:
+        if bg_color in widget.vflags:
+            bg = widget.vflags[bg_color]
+        else:
+            raise ValueError('Background color is not found')
+    else:
+        bg = None
+    text_surface = font.render(valpack.Text, text_Is_Antialias in widget.uflags, valpack.Color, bg)
+    screen.blit(text_surface, widget.get_pos())
