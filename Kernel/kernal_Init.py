79712ed75f65f61b7020d43a2e19ee6ca6a15802
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-01-28 22:03:07 +0700

    update to auto stop fill when screen was filled before

--- Kernel/kernal_Init.py
+++ Kernel/kernal_Init.py
@@ -1,8 +1,17 @@
 import pygame
 init_state = False
+_fill_mode = True
+
+def set_fill_mode(mode=False):
+    global _fill_mode
+    _fill_mode = mode
+
+def should_fill():
+    return _fill_mode
 def init():
     global init_state
     if not init_state:
         pygame.init()
         init_state = True
+    set_fill_mode()
 
