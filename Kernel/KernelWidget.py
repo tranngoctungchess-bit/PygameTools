d3e85f899b5f41cf5d62650a2f8e22f0d035b9b7
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-01-31 14:42:14 +0700

    Update Widget

--- Kernel/KernelWidget.py
+++ Kernel/KernelWidget.py
@@ -6,9 +6,15 @@ import pygame
 from Kernel.ObjType import MathVal2, MathVal1
 from Kernel.UFlags import *
 from Kernel.VFlags import *
-class ValuePackTypeError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
+def valid_background(bg):
+    """
+    Check if it is a valid background
+    """
+    if isinstance(bg, pygame.Surface) or (
+            isinstance(bg, tuple) and len(bg) in (3, 4) and all(isinstance(x, (int, float)) for x in bg)):
+        return True
+    else:
+        return False
 """
 ImmutableRect and MutableRect:
 the base object for any widget class as bar, button coming soon in this tool
@@ -23,9 +29,21 @@ class MutableRect:
     y:Union[int,float]
     w:Union[int, float]
     h:Union[int,float]
+class MainScreen:
+    """
+
+    """
+    def __init__(self, size, flags=0):
+        self.surface = pygame.display.set_mode(size, flags)
+        self.background = None
+    def fill(self, color):
+        self.surface.fill(color)
+        self.background = color
+    def blit(self, source, dest):
+        self.surface.blit(source, dest)
 class Widget:
     """
-    Docstring for Widget
+    parent: the base contains this widget
     rect: the rect of this widget
     is_dirty: whether this widget is changed
     uflags: the unchangeable flags set for this widget
@@ -34,10 +52,13 @@ class Widget:
     dirty_vflags: the valueable flags set for this widget
     pos_x: the x positions list for all objects in this widget
     pos_y: the y positions list for all objects in this widget
+    temp_bg: the bg of widget parent, if widget parent is pygame.Surface
+    child: the dictionary of widget child
     """
-    __slots__ = ('rect', 'is_dirty', 'uflags', 'vflags', 'dirty_uflags', 'dirty_vflags', 'pos_x', 'pos_y')
-    def __init__(self, rect: MathVal1, can_change = False):
+    __slots__ = ('parent' ,'rect', 'is_dirty', 'uflags', 'vflags', 'dirty_uflags', 'dirty_vflags', 'pos_x', 'pos_y', 'temp_bg', 'children')
+    def __init__(self, parent, rect: MathVal1, bg=None, can_change = False):
         self.rect = ImmutableRect(*rect) if not can_change else MutableRect(*rect)
+        self.parent = parent
         self.is_dirty = False
         self.uflags = set()
         self.vflags = {}
@@ -45,6 +66,21 @@ class Widget:
         self.dirty_vflags = set()
         self.pos_x = []
         self.pos_y = []
+        self.children = {}
+        if bg is not None and not valid_background(bg):
+            raise ValueError("Invalid background format")
+        self.temp_bg = bg
+    def get_background(self):
+        if isinstance(self.parent, Widget):
+            return self.parent.get_background()
+        elif isinstance(self.parent, pygame.Surface):
+            if self.temp_bg is None:
+                raise ValueError("Widget with pygame.Surface parent must provide bg color in __init__")
+            return self.temp_bg
+        elif isinstance(self.parent, MainScreen):
+            return self.parent.background
+        else:
+            raise ValueError("Your widget parent must be the widget, pygame.Surface or MainScreen class")
     def add_obj(self, pos: MathVal2):
         self.pos_x.append(pos[0])
         self.pos_y.append(pos[1])
@@ -54,10 +90,36 @@ class Widget:
         return self.rect.x, self.rect.y
     def get_size(self):
         return self.rect.w, self.rect.h
+    def get_surface(self):
+        if isinstance(self.parent, pygame.Surface):
+            return self.parent
+        elif isinstance(self.parent, MainScreen):
+            return self.parent.surface
+        else:
+            return self.parent.get_surface()
+    def add_child(self, widget, name):
+        self.children[name] = widget
+    def destroy(self):
+        self.hide_itself()
+        if isinstance(self.parent, Widget):
+            for name, child in self.parent.children.items():
+                if child is self:
+                    del self.parent.children[name]
+                    break
+        self.parent = None
+        self.children.clear()
+    def hide_itself(self):
+        bg = self.get_background()
+        if isinstance(bg, tuple):
+            surface = self.get_surface()
+            pygame.draw.rect(surface, bg, self.rect)
+        elif isinstance(bg, pygame.Surface):
+            surface = self.get_surface()
+            surface.blit(bg, self.rect.topleft, self.rect)
     def inrect(self, pos: MathVal2):
         px, py = pos
         return (self.rect.x <= px <= self.rect.x + self.rect.w) and (self.rect.y <= py <= self.rect.y + self.rect.h)
-    def set_flags(self, uflags: tuple=(), vflags: tuple=()):
+    def set_flags(self, uflags: tuple=(), vflags: Tuple[tuple]=(())):
         for uflag in uflags:
             self.uflags.add(uflag)
             self.dirty_uflags.add(uflag)
@@ -113,18 +175,30 @@ def convert_a_lot(widget: Widget):
     Y = np.array(widget.pos_y)
     result_pos = (X + widget.rect.x, Y + widget.rect.y)
     return result_pos
-def createpairpack(Objname,valname1, valname2):
-    return namedtuple(Objname, [valname1, valname2])
 class PygameRender:
-    def __init__(self, widget: Widget, base_screen):
+    """
+
+    """
+    def __init__(self, widget: Widget):
         self.widget = widget
-        self.base_screen = base_screen
+        if isinstance(self.widget.parent, Widget):
+            self.bg = self.widget.parent.get_background()
+        elif isinstance(self.widget.parent, pygame.Surface):
+            self.bg = self.widget.temp_bg
+        elif isinstance(self.widget.parent, MainScreen):
+            self.bg = self.widget.parent.background
+        else:
+            raise ValueError("Your widget parent must be the widget, pygame.Surface or MainScreen class")
     def render(self):
         from Kernel.PygameRender.LinkRenderfunc import renderfunc
-        for flag in list(self.widget.dirty_vflags):
-            if flag in renderfunc:
-                renderfunc[flag](self.widget, self.base_screen)
-                pygame.display.flip()
+        try:
+            if self.widget.is_dirty:
+                for flag in list(self.widget.dirty_vflags):
+                    renderfunc[flag](self.widget, self.widget.get_surface())
+                    pygame.display.flip()
+            self.widget.is_dirty = False
+        except KeyError as e:
+            raise ValueError(f"Flag {e} not found in renderfunc mapping. Check LinkRenderfunc.py")
 class SkiaRender:
     def __init__(self, widget: Widget):
         self.widget = widget
