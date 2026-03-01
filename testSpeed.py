10ec1aed18af2094d7793ab52d03d188159a23e8
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-03-01 22:26:04 +0700

    .

--- testSpeed.py
+++ testSpeed.py
@@ -5,7 +5,8 @@ screen = pygame.display.set_mode((800,600), pygame.RESIZABLE)
 running = True
 while running:
     for event in pygame.event.get():
-        print(event)
+        if event.type == pygame.VIDEORESIZE:
+            print('resize')
         if event.type == pygame.QUIT:
             running = False
         screen.fill((0,0,0))
