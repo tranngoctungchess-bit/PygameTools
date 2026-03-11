bac11275cedf6cb3cc68d745bf5783305b651e38
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-03-11 19:20:06 +0700

    .

--- Kernel/ObjType.py
+++ Kernel/ObjType.py
@@ -1,8 +1,6 @@
 from typing import Tuple, Union
 from collections import namedtuple
-def createpairpack(Objname,valname1, valname2):
-    return namedtuple(Objname, [valname1, valname2])
 MathVal1 = Tuple[Union[int, float],Union[int, float],Union[int, float],Union[int, float]]
 MathVal2 = Tuple[Union[int, float],Union[int, float]]
-Border = createpairpack('Border', 'border_width', 'border_col')
+Border = namedtuple('Border', ['border_width', 'border_col'])
 TextPack = namedtuple('Text', ['Color', 'Font', 'Size', 'Text'])
