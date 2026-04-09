beea46d198e0e39961b90ae79461041ecdc320c7
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-04-09 13:11:51 +0700

    .

--- Kernel/PgRenderCompo/__init__.py
+++
@@ -1,5 +0,0 @@
-from Kernel.PgRenderCompo.LinkRenderfunc import *
-from Kernel.PgRenderCompo.RRender import *
-from Kernel.PgRenderCompo.Render import *
-from Kernel.PgRenderCompo.TextObj import *
-from Kernel.PgRenderCompo.ButtonObj import *
