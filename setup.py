7dfb873cd252beb6cb6f80c0d859db9e8934cadb
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-04-09 12:55:06 +0700

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PGTKernelbasic",
-    version="0.23.1",
+    version="0.23.2",
     author="Your Name",
     description="Lightweight UI framework for Pygame",
     packages=find_packages(),
