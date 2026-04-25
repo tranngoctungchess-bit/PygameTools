a85a0e98b867ceed265f50ba959127e38ed151ce
tranngoctungchess-bit <tranngoctung.chess@gmail.com>
2026-04-25 21:55:25 +0700

    .

---
+++ test/test.py
@@ -0,0 +1,60 @@
+from pgtkb import (
+    MainApplication, Label, FixedButton, text_Is_Antialias, Anchor,
+    pressed_bg, ImagePack, Uplclick, Audio, Downlclick
+)
+import pygame
+
+# --------------------------
+# Âm thanh và ảnh
+# --------------------------
+baihat = Audio('hb.mp3')
+baihat.set_volume(0.2)
+
+mainapp = MainApplication((800, 600), caption="Chương trình sinh nhật")
+
+natrue_start = ImagePack("startbg.png")
+natrue_start.resize((800, 600))
+flower = ImagePack("flower.png")
+flower.resize((800, 600))
+
+# --------------------------
+# Giao diện ban đầu
+# --------------------------
+mainapp.screen.change_bg(natrue_start.get_image())
+mainapp.screen.set_margin(border_percent=(30, 10))
+
+hellolabel = Label(
+    mainapp.screen,
+    (0, 0, 0),
+    24,
+    "Màn hình chờ",
+)
+hellolabel.goto_margin(Anchor.topcenter)
+
+startbutton = FixedButton(
+    mainapp.screen,
+    (200, 75),
+    (255, 0, 100),
+    pressbg=(255, 0, 255)
+)
+startbutton.goto_margin(Anchor.bottomcenter)
+startbutton.set_margin()
+
+startlabel = Label(
+    startbutton,
+    (0, 0, 0),
+    24,
+    "Start"
+)
+startlabel.goto_margin(Anchor.center)
+@startbutton.on_event(Uplclick)
+def play_happybirthday():
+    mainapp.screen.blank_newbg(flower.surface)
+    baihat.set_volume(0.5)
+    baihat.play()
+    event_manage = mainapp.event_manager
+    event_manage.add_kmod_func((pygame.KMOD_CTRL, pygame.KMOD_ALT),func= baihat.pause)
+    event_manage.add_kmod_func((pygame.KMOD_CTRL,),pygame.K_r ,baihat.resume)
+    event_manage.add_kmod_func((pygame.KMOD_CTRL,),pygame.K_a ,baihat.play)
+    event_manage.add_kmod_func((pygame.KMOD_CTRL,),pygame.K_s ,baihat.stop)
+mainapp.threadstart()
